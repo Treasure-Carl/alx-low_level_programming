78922f5050c14478f43731a53b1738cac734a631
Treasure-Carl <you@treasureolayinkapaschal@gmail.com>
2022-07-01 11:32:03 -0700

    My weird number 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+/
+
+ * main - entry point,
+ *
+ *  *
+ *
+ *   * Return:  0
+ *
+ *    */
+
+
+
+ int main(void)
+
+{
+
+	  puts("\"Programming is like building a multilingual puzzle");
+
+	    return (0);
+
+}
