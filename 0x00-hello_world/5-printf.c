436fa40d28fc68f1714bff8e32af0b17fbffbb9d
Treasure-Carl <you@treasureolayinkapaschal@gmail.com>
2022-07-01 11:44:36 -0700

    I hope i am correct

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/
+ *main - prints some text
+ *
+ * Return: always 0(success)
+ */
+
+
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
