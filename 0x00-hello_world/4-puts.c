99ac19231dffa2646d4cdf7b650dcbe6d9c4511c
Treasure-Carl <treasureolayinkapaschal@gmail.com>
2022-09-08 13:50:40 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
+#include <stdio.h>
 /**
 * main - print the string in the put function 
 *
 * Description: using the main function
-*
 * this program prints "programming is like building a multilingual puzzle 
 *Return: 0
 */
