666afb486420287034d7005a97de2baec903f902
Treasure-Carl <treasureolayinkapaschal@gmail.com>
2022-09-08 08:58:06 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
 
-
-
 /
-
  * main - entry point,
  *
- *  *
- *
- *   * Return:  0
- *
- *    */
-
+ * Return: 0
+ */
 
 
  int main(void)
-
 {
-
-	  puts("\"Programming is like building a multilingual puzzle");
-
-	    return (0);
-
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
