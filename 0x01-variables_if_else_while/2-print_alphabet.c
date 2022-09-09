16d546e97284755888af13cb9e59bf87a30bb894
Treasure-Carl <treasureolayinkapaschal@gmail.com>
2022-09-09 15:06:23 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char love;
+
+	for (love = 'a'; love <= 'z'; love++)
+	putchar(love);
+	putchar('\n');
+	return (0);
+
+}
