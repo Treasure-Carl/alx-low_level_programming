cf8d515249763bbd41110dd8cae83450e42b74fa
Treasure-Carl <treasureolayinkapaschal@gmail.com>
2022-09-08 22:56:24 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints a test
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("ith proper grammer, gut the outcome is a piece of art,\n");
+	return (0);
+}
+
