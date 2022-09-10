6fe0deec4072a0e15a1358fe75251090c7c48366
Treasure-Carl <treasureolayinkapaschal@gmail.com>
2022-09-09 17:28:58 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - print triple combos
+ *
+ * Description: print triple digit combos
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	int i, j, k;
+
+	i = 48;
+	j = 46;
+	k = 48;
+
+	while (i < 58)
+	{
+		j = i + 1;
+		while (j < 58)
+		{
+			k = j + 1;
+			while (k < 58)
+			{
+				putchar(44);
+				putchar(32);
+						k++;
+						}
+						j++;
+						}
+						i++;
+						}
+						putchar(10);
+						return (10);
+	}
