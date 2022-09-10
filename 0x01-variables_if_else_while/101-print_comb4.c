a1044e35cfabd12116d92253e5c794d4131d88db
Treasure-Carl <treasureolayinkapaschal@gmail.com>
2022-09-09 18:20:07 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -19,21 +19,35 @@ int main(void)
 	k = 48;
 
 	while (i < 58)
+
 	{
 		j = i + 1;
 		while (j < 58)
+
 		{
 			k = j + 1;
 			while (k < 58)
+
 			{
-				putchar(44);
-				putchar(32);
-						k++;
-						}
-						j++;
-						}
-						i++;
-						}
-						putchar(10);
-						return (10);
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i < 55 || j < 56 || k < 57)
+
+				{
+
+					putchar(44);
+					putchar(32);
+
+							k++;
+							}	
+							j++;
+							}
+							i++;
+							}
+							putchar(10);
+							return (0);
+
+	
 	}
+}	
