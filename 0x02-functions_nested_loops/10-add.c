4b8e0b1e269fb2b48ad167ad65b50011d57a33cb
Treasure-Carl <treasureolayinkapaschal@gmail.com>
2022-09-13 16:56:58 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+
+/**
+ * add - add two numbers.
+ *
+ * @a: first number
+ * @b: second number
+ * Return: Always 0.
+ */
+
+int add(int a, int b)
+
+{
+
+		int sum;
+
+		sum = a + b;
+
+		return (sum);
+
+}
