5593533b59a15ce1b545276ce7ad8e49b0d81ff3
Treasure-Carl <treasureolayinkapaschal@gmail.com>
2022-09-13 16:00:35 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+
+/**
+ * _abs - compute the absolute value.
+ *
+ * @n: to be checked
+ * Return: Always 0.
+ */
+
+int _abs(int n)
+{
+
+		if (n < 0)
+		{
+			n = (-1) * n;
+
+		}
+		return (n);
+
+}
