69ded69037cc82ea7c37d445e5ffd872da1a932e
Treasure-Carl <treasureolayinkapaschal@gmail.com>
2022-09-13 13:52:51 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+
+/**
+ * main - debugging example
+ *
+ * Return: 0
+ */
+
+int main(void)
+
+{
+
+		char *holberton = "Holberton";
+
+		while (*holberton)
+		{
+
+		_putchar(*holberton);
+		holberton++;
+
+		}
+		_putchar('\n');
+
+		return (0);
+
+}
