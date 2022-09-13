1328b650f3a2e562f7869f8102fb6fd3c3f0e372
Treasure-Carl <treasureolayinkapaschal@gmail.com>
2022-09-13 14:19:48 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+
+
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase
+ * followed by new line.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+
+{
+
+		int i;
+		int j;
+
+		for (i = '0'; i <= '9'; i++)
+		{
+			for (j = 'a'; j <= 'z'; j++)
+
+		{
+			_putchar(j);
+
+		}
+			_putchar('\n');
+
+		}
+
+}
