9cbcc669347aba399180ebdc8859ad8bc7001fb8
Treasure-Carl <treasureolayinkapaschal@gmail.com>
2022-09-14 05:47:31 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,31 +1,63 @@
-#include <stdio.h>
+#include "main.h"
 
 
 
 /**
- * main - program execution entry point
- * Description - print the sum of all multipliers of 3 or 5 below 1024
- *
- * Return: Always 0 on success
+ * print_times_table - time table function
+ * @n: integer to return
+ * Return: always return n
  */
 
-
-
-int main(void)
+void print_times_table(int n)
 {
+	int i;
+	int j;
 
-		int i;
-		int sum = 0;
+	if (n <= 15 && n >= 0)
+	{
 
-		for (i = 0; i < 1024; i++)
+		for (i = 0; i <= n; j++)
 		{
+			for (i = 0; i <= n; j++)
+			{
 
-			if ((i % 3 == 0) || (i % 5 == 0))
-				sum += i;
+				int prod = j * i;
+
+				if (j == 0)
+
+				{
+					_putchar('0');
+				} else if (prod <= 9)
+				{
+
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + prod);
+				} else if (prod > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar('0' + (prod / 100));
+					_putchar('0' + ((prod / 10) % 10));
+					_putchar('0' + (prod % 10));
+
+				} else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + (prod / 10));
+					_putchar('0' + (prod % 10));
+
+				}
+
+			}
+			_putchar('\n');
 
 		}
-		printf("%d\n", sum);
 
-		return (0);
+	}
 
 }
