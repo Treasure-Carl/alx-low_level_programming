ddc4ad090422f99486e2fa4b4cba10c5b2f1f5a4
Treasure-Carl <treasureolayinkapaschal@gmail.com>
2022-09-15 02:09:54 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * print_line - check the code for Holberton School students.
+ *
+ * @n: number of times to print _.
+ */
+
+void print_line(int n)
+{
+	while (n > 0)
+	{
+
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n');
+
+}
