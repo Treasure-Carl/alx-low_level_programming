e75d9d6b600e1ebb3ddf8cbfa67de71a07e8f9c5
Treasure-Carl <treasureolayinkapaschal@gmail.com>
2022-09-15 13:51:01 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+#include <stdio.h>
+
+
+
+/**
+ * main - fizzbuzz algorithm.
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz");
+
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf("Buzz");
+
+		}
+		else if (i % 15 == 0)
+		{
+			printf("FizzBuzz");
+
+		}
+		else
+		{
+			printf("%d", i);
+
+		}
+		if (i == 100)
+		{
+			break;
+
+		}
+		else
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+
+}
