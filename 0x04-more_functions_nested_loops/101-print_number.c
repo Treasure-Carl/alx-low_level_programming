064cd2b82209702a6071f49d3fed0e771c47e07a
Treasure-Carl <treasureolayinkapaschal@gmail.com>
2022-09-15 14:24:44 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+ * print_number - prints an interger.
+ *
+ * @n: number.
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+
+	}
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
+
+}
