7f5f6ea7cbf466bfa18cccbb421ce4f7732018b9
Treasure-Carl <treasureolayinkapaschal@gmail.com>
2023-07-19 13:16:59 -0700

    The task stated that it's not doing a line count, well i guess that can't be trusted

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
  * largest_number - returns the largest of 3 numbers
  * @a: first integer
@@ -10,41 +8,24 @@
  * Return: largest number
  */
 
-
-
 int largest_number(int a, int b, int c)
-
 {
 
 	int largest;
 
-
-
 	if (a > b && a > c)
-
 	{
-
 		largest = a;
-
 	}
-
 	else if (b > a && b > c)
-
 	{
-
 		largest = b;
-
 	}
-
 	else
-
 	{
-
 		largest = c;
-
 	}
 
 	return (largest);
 
 }
-
