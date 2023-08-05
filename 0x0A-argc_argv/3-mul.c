632c3ba4d3553bfaa514dffff6703cf9dbdbe2d2
Treasure-Carl <treasureolayinkapaschal@gmail.com>
2023-08-05 09:03:31 -0700

    made an update

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,7 +5,7 @@
  * main - Prints the multiplication of two numbers, followed by a new line.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
- *
+ * @prod - just a global variable that prints the result of the product
  * Return: If the program receives two arguments - 0.
  *         If the program does not receive two arguments - 1.
  */
